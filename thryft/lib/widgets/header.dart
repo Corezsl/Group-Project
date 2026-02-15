2335faa500c34d41c22b787080f814fd2cc1d1fc
AbigailHinchliffe <abigail.hinchliffe@gmail.com>
2026-02-15 15:53:08 +0000

    Added spacing and account icon to header

--- thryft/lib/widgets/header.dart
+++ thryft/lib/widgets/header.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key});
@@ -10,10 +11,25 @@ class Header extends StatelessWidget {
       height: 100,
       color: const Color.fromARGB(255, 71, 164, 245),
       child: const Center(
-        child: Text(
-          'Placeholder for header',
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 14, fontWeight: FontWeight.bold),
+        child: Row(
+          children:[
+            Spacer(),
+            Text(
+              'Placeholder for header',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: IconButton(
+                icon: Icon (Icons.person_outline,
+                color: Colors.white
+                ),
+                onPressed: null,
+              ),
+            ),
+          ],
         ),
       ),
     );
