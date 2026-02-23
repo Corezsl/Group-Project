38278d61a752eb411a378a2d396e33cc06ac0460
Alucard <alucardw@duck.com>
2026-02-23 15:50:35 +0000

    Revert "Merge branch 'main' of https://github.com/Corezsl/Group-Project"
    This reverts commit 3ecca92049c0d7a19231b29dba42508ffa33f93b, reversing
    changes made to 35567086b7254c3809516cd063803d6ac5c3e98b.

--- thryft/lib/widgets/header.dart
+++ thryft/lib/widgets/header.dart
@@ -1,136 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context) {
+
     return Container(
       height: 100,
       color: const Color.fromARGB(255, 71, 164, 245),
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
+          children:[
             Row(
               children: [
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Image.asset(
-                        'assets/images/thyrft_logo.png',
-                        height: 60,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+            Image.asset(
+              'assets/images/thyrft_logo.png',
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+            Spacer(),
+            SizedBox(
+              width: 500,
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: Colors.white),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: Colors.white70),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
                   ),
+                  filled: true,
+                  fillColor: Color.fromARGB(50, 255, 255, 255),
                 ),
-                SizedBox(
-                  width: 500,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search, color: Colors.white),
-                      hintText: 'Search',
-                      hintStyle: TextStyle(color: Colors.white70),
-                      contentPadding: EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 8,
-                      ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none),
-                      filled: true,
-                      fillColor: Color.fromARGB(50, 255, 255, 255),
-                    ),
-                    style: TextStyle(color: Colors.white),
-                  ),
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Spacer(),
+            FilledButton.tonal(
+              onPressed: null,
+              style: FilledButton.styleFrom(
+                textStyle: TextStyle(fontSize: 16.0),
+              ),
+              child: Text('SELL NOW'),
+            ),
+            Spacer(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon (Icons.favorite_outline,
+                  color: Colors.white
                 ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FilledButton.tonal(
-                        onPressed: null,
-                        style: FilledButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 16.0),
-                        ),
-                        child: Text('SELL NOW'),
-                      ),
-                      const SizedBox(width: 16),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: Icon(
-                              Icons.favorite_outline,
-                              color: Colors.white,
-                            ),
-                            onPressed: null,
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.shopping_cart_outlined,
-                              color: Colors.white,
-                            ),
-                            onPressed: null,
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.person_outline,
-                              color: Colors.white,
-                            ),
-                            onPressed: () => context.push('/account'),
-                          ),
-                          const SizedBox(width: 16),
-                        ],
-                      ),
-                    ],
-                  ),
+                onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon (Icons.shopping_cart_outlined,
+                  color: Colors.white
+                ),
+                onPressed: null,
+                ),
+                IconButton(
+                icon: Icon (Icons.person_outline,
+                color: Colors.white
+                ),
+                onPressed: null,
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () => context.go('/'),
-                  style: TextButton.styleFrom(foregroundColor: Colors.white),
-                  child: Text('Home'),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: null,
+              child: Text('Home'),
+              ),
+            SizedBox(width: 16),
+            TextButton(
+              onPressed: null,
+              child: Text('Shirts'),
                 ),
-                SizedBox(width: 16),
-                TextButton(
-                  onPressed: null,
-                  child: Text('Shirts', style: TextStyle(color: Colors.white)),
+            SizedBox(width: 16),
+            TextButton(
+              onPressed: null,
+              child: Text('Trousers'),
                 ),
-                SizedBox(width: 16),
-                TextButton(
-                  onPressed: null,
-                  child: Text(
-                    'Trousers',
-                    style: TextStyle(color: Colors.white),
-                  ),
+            SizedBox(width: 16),
+            TextButton(
+              onPressed: null,
+              child: Text('Shoes'),
                 ),
-                SizedBox(width: 16),
-                TextButton(
-                  onPressed: null,
-                  child: Text('Shoes', style: TextStyle(color: Colors.white)),
+            SizedBox(width: 16),
+            TextButton(
+              onPressed: null,
+              child: Text('Accessories'),
                 ),
-                SizedBox(width: 16),
-                TextButton(
-                  onPressed: null,
-                  child: Text(
-                    'Accessories',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(width: 16),
-                TextButton(
-                  onPressed: null,
-                  child: Text(
-                    'About Us',
-                    style: TextStyle(color: Colors.white),
-                  ),
+            SizedBox(width: 16),
+            TextButton(
+              onPressed: null,
+              child: Text('About Us'),
                 ),
               ],
             ),
