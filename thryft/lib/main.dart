4a36a03cb5d456d70774385dcd725f20847effc1
Alucard <alucardw@duck.com>
2026-02-23 15:23:18 +0000

    Updated main

--- thryft/lib/main.dart
+++ thryft/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:thryft/router.dart';
+import 'package:thryft/widgets/header.dart';
+import 'package:thryft/widgets/product_carousel.dart';
+import 'package:thryft/widgets/category_section.dart';
 
 void main() {
   runApp(const ThryftApp());
@@ -10,7 +12,7 @@ class ThryftApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Thryft',
       theme: ThemeData(
@@ -22,7 +24,51 @@ class ThryftApp extends StatelessWidget {
           primary: const Color.fromARGB(255, 71, 164, 245),
         ),
       ),
-      routerConfig: router,
+      home: const HomePage(),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Header(),
+            
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "Shop by Category",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 12),
+            const CategorySection(),
+
+            const SizedBox(height: 24),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "New Arrivals",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 12),
+            // other widgets can be added here
+            const SizedBox(height: 16),
+            const ProductCarousel(),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
