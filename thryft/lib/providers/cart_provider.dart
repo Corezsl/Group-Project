fb4405fe15ea0bf2d98ce061a4d2a1c398198027
George Papageorgiou <g.papageorgiou005@gmail.com>
2026-03-21 14:10:02 +0000

    remove cart quantities and replace stepper with trash icon

--- thryft/lib/providers/cart_provider.dart
+++ thryft/lib/providers/cart_provider.dart
@@ -14,31 +14,15 @@ class CartProvider extends ChangeNotifier {
 
   void addItem(Product product) {
     final index = _items.indexWhere((i) => i.product.id == product.id);
-    if (index >= 0) {
-      _items[index] = _items[index].copyWith(
-        quantity: _items[index].quantity + 1,
-      );
-    } else {
+    if (index < 0) {
       _items.add(CartItem(product: product));
+      notifyListeners();
     }
-    notifyListeners();
   }
 
   void removeItem(String productId) {
     _items.removeWhere((i) => i.product.id == productId);
     notifyListeners();
-  }
-
-  void updateQuantity(String productId, int quantity) {
-    if (quantity <= 0) {
-      removeItem(productId);
-      return;
-    }
-    final index = _items.indexWhere((i) => i.product.id == productId);
-    if (index >= 0) {
-      _items[index] = _items[index].copyWith(quantity: quantity);
-      notifyListeners();
-    }
   }
 
   void clear() {
