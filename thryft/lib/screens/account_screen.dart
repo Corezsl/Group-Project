986e585893c5a37f57c356522b50143c5e935ef6
ChrystallaT1 <chrystallatambouri@hotmail.com>
2026-04-10 14:18:20 +0300

    add missing app_drawer imports to screens after header refactor

--- thryft/lib/screens/account_screen.dart
+++ thryft/lib/screens/account_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thryft/widgets/footer.dart';
+import 'package:thryft/widgets/app_drawer.dart';
 import 'package:thryft/widgets/header.dart';
 
 class AccountScreen extends StatelessWidget {
--- thryft/lib/screens/contact_screen.dart
+++ thryft/lib/screens/contact_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thryft/utils/responsive.dart';
 import '../widgets/footer.dart';
+import '../widgets/app_drawer.dart';
 import '../widgets/header.dart';
 
 /// Screen providing a contact form for users to send messages to the team.
